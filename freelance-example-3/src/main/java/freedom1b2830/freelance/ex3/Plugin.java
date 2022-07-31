9396b5af42e30d679c07903f0f6947cecaa14d3b
freedom1b2830 <freedom1b2830@gmail.com>
2022-07-31 04:24:08 +0000

    test3

--- freelance-example-3/src/main/java/freedom1b2830/freelance/ex3/Plugin.java
+++ freelance-example-3/src/main/java/freedom1b2830/freelance/ex3/Plugin.java
@@ -1,12 +1,36 @@
 package freedom1b2830.freelance.ex3;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class Plugin extends JavaPlugin implements Listener {
-	@Override
-	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(this, this);
-	}
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+
+    @EventHandler
+    public void entityKilled2(EntityDamageByEntityEvent event) {
+        @NotNull Entity entity = event.getEntity();
+        if (entity instanceof LivingEntity) {
+            LivingEntity livingEntity = (LivingEntity) entity;
+            double health = livingEntity.getHealth();
+            if (health <= 0D) {
+                @NotNull Entity damager = event.getDamager();
+                if (damager instanceof Player) {
+                    @NotNull String playerName = ((Player) damager).getName();
+                    getLogger().info(String.format("%s killed by %s ", livingEntity.getName(), playerName));
+                }
+            }
+        }
+
+    }
 }
